25db497fea30f6430a8d53d6fbadf73d76985a67
unknown <mylenebarber8@gmail.com>
2024-03-13 11:16:22 +0800

    first commit

--- login.js
+++ login.js
@@ -1,17 +1,23 @@
-function login(){
-    var username = document.getElementById("user").value;
-    var password = document.getElementById("pass").value;
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-    if(username === "" || password === ""){
+    var real_username = "myelbarber";
+    var real_password = "mylene";
 
-        document.getElementById("error").innerHTML =("All textboxes are required")
+    if (username === "" || password === "") {
+
+        document.getElementById("error").innerHTML = "All textboxes are required";
     }
 
-    else{
-        document.getElementById("error").innerHTML= "Welcome " + username;
+    else (username === real_username && password === real_password) {
+        document.getElementById("error").innerHTML = "Welcome " + username;
     }
 }
 
+    else {
+    document.getElementById("error").innerHTML = "Invalid username or password";
+}
 function forgot() {
     alert("You clicked forgot password");
 }
